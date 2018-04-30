98e2d5f581495399f0db7f214b8de24de9d026bb
Sergio Cuadrado Vereau <sergiocv97@gmail.com>
2018-04-30 13:53:58 +0200

    Commit cambios login 1.0

--- app/src/main/java/com/team/a404/a404team/LoginActivity.java
+++ app/src/main/java/com/team/a404/a404team/LoginActivity.java
@@ -1,18 +1,28 @@
 package com.team.a404.a404team;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.support.design.widget.Snackbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     private FirebaseAuth firebaseAuth;
     private EditText user,pass;
     private Button login;
-    private TextView noCuenta;
+    private TextView noCuenta,nopass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +31,72 @@ public class LoginActivity extends AppCompatActivity {
         user = (EditText) findViewById(R.id.usuario);
         pass = (EditText) findViewById(R.id.pass1);
         noCuenta = (TextView) findViewById(R.id.nocuenta);
+        nopass = (TextView)findViewById(R.id.nopass);
         login = (Button) findViewById(R.id.logbutt);
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+            finish();
+//            Intent intent1 = new Intent(LoginActivity.this, MainScreen.class);
+//            startActivity(intent1);
+
+        }
+        noCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+//                startActivity(intent);
+            }
+        });
 
     }
+
+    @Override
+    public void onClick(View view) {
+        closeSoftKeyBoard();
+        if (view == login) {
+            userLogin();
+        }
+        if (view == nopass){
+//            Intent intent = new Intent(LoginActivity.this, ForgetPwd.class);
+//            startActivity(intent);
+        }
+    }
+    public void closeSoftKeyBoard() {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(login.getWindowToken(), 0);
+    }
+    private void userLogin() {
+        String usr = user.getText().toString().trim();
+        String pwd = pass.getText().toString().trim();
+
+
+        if (TextUtils.isEmpty(usr)) {
+            // email is empty
+            Snackbar.make(login, getString(R.string.meteuser), Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            return;
+            // para la ejecucion
+        }
+        if (TextUtils.isEmpty(pwd)) {
+            // password is empty
+            Snackbar.make(login, getString(R.string.metepass), Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            // para la ejecucion
+            return;
+        }
+
+        firebaseAuth.signInWithEmailAndPassword(usr, pwd)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            finish();
+//                            Intent intent1 = new Intent(LoginActivity.this, MainScreen.class);
+//                            startActivity(intent1);
+                        }else{
+                            Snackbar.make(login, getString(R.string.nolog), Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                        }
+
+                    }
+                });
+    }
 }
+
