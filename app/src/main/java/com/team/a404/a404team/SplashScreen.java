ac0a56ecafb2ab6dbaa80cae589afbdc93a05378
Borja <borjalagarejo@gmail.com>
2018-05-05 15:22:36 +0200

    Antes del login

--- app/src/main/java/com/team/a404/a404team/SplashScreen.java
+++ app/src/main/java/com/team/a404/a404team/SplashScreen.java
@@ -1,8 +1,12 @@
 package com.team.a404.a404team;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,10 +14,27 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        Thread t = new Thread(){
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void run() {
+                try {
+                    //lottieAnimationView = (LottieAnimationView) findViewById(R.id.animation_view);
+                    //lottieAnimationView.playAnimation();
+                    sleep(1500);
+                }catch (Exception e){
+
+                } finally {
+                    Intent myIntent = new Intent(SplashScreen.this, tipo_login.class);
+                    myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(myIntent);
+                }
+            }
+        };
+        t.start();
 
     }
 }
