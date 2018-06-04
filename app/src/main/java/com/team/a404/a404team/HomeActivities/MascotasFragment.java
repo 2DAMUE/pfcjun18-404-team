359cabcbfb6ab4c4ffd62d8273efac53d08e9d34
Sergio Cuadrado <sergiocv97@gmail.com>
2018-06-04 02:24:40 +0200

    Commit fotos mascotas

--- app/src/main/java/com/team/a404/a404team/HomeActivities/MascotasFragment.java
+++ app/src/main/java/com/team/a404/a404team/HomeActivities/MascotasFragment.java
@@ -2,9 +2,16 @@ package com.team.a404.a404team.HomeActivities;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,21 +21,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.mikhaellopez.circularimageview.CircularImageView;
 import com.team.a404.a404team.Datos.AnuncioInformation;
 import com.team.a404.a404team.R;
 
 import org.w3c.dom.Text;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import static android.app.Activity.RESULT_OK;
 
 public class MascotasFragment extends Fragment {
     private FloatingActionButton nuevaMascota;
@@ -36,10 +53,13 @@ public class MascotasFragment extends Fragment {
     private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     private DatabaseReference DataRef;
     private EditText nombre, raza, descripcion;
-    private CircularImageView image;
+    private CircularImageView imageNueva;
     private Button saveButton;
     private ArrayList<AnuncioInformation> informacionMascotas = new ArrayList<>();
     private ArrayList<String> nombreMascotas = new ArrayList<>();
+    private String nom_mascota;
+    private static final int PICK_IMAGE_REQUEST = 100;
+    Uri imageUri;
 
     public MascotasFragment() {
         // Required empty public constructor
@@ -96,6 +116,7 @@ public class MascotasFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 final Dialog dialog = new Dialog(getContext());
+                nom_mascota = "";
                 dialog.setContentView(R.layout.dialog_nueva_macota);
                 dialog.show();
                 final DatabaseReference datosMascota = FirebaseDatabase.getInstance().getReference("usuarios")
@@ -104,6 +125,17 @@ public class MascotasFragment extends Fragment {
                 raza = (EditText)dialog.findViewById(R.id.raza_m);
                 descripcion = (EditText)dialog.findViewById(R.id.descripcion_m);
                 saveButton = (Button) dialog.findViewById(R.id.btn_enviar);
+                imageNueva = (CircularImageView)dialog.findViewById(R.id.img_nuevamascota);
+                imageNueva.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        nom_mascota = nombre.getText().toString().trim();
+                        if(TextUtils.isEmpty(nom_mascota)){
+                            Toast.makeText(getContext(), "Introduzca primero los datos de la mascota.", Toast.LENGTH_SHORT).show();
+                        }else{
+                        OpenGallery();}
+                    }
+                });
                 saveButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -121,5 +153,42 @@ public class MascotasFragment extends Fragment {
             }
         });
         return rootView;
+    }
+    private void OpenGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+    @Override
+    public void onActivityResult(int requestcode, int resultcode, Intent data) {
+        super.onActivityResult(requestcode, resultcode, data);
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getInstance().getReference().child("images").child(firebaseAuth.getCurrentUser().getUid())
+                .child("mascotas").child(nom_mascota).child(nom_mascota+".jpg");
+        if (resultcode == RESULT_OK && requestcode == PICK_IMAGE_REQUEST) {
+            imageUri = data.getData();
+            nuevaMascota.setImageURI(imageUri);
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos);
+            byte[] img = baos.toByteArray();
+            UploadTask uploadTask = storageRef.putBytes(img);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    DataRef.child(nom_mascota).child("url_foto").setValue(downloadUrl.toString());
+                    Log.d("downloadUrl-->", "" + downloadUrl); }
+            });
+        }
     }
 }
