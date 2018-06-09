7d4aeac0bce23aad85ed940c1cdd9a531e373b2d
carlos <compalortizmenendez@gmail.com>
2018-06-09 16:35:01 +0200

    commit singIn con google

--- app/src/main/java/com/team/a404/a404team/Zona_lobby/lobby.java
+++ app/src/main/java/com/team/a404/a404team/Zona_lobby/lobby.java
@@ -3,24 +3,52 @@ package com.team.a404.a404team.Zona_lobby;
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.team.a404.a404team.HomeActivities.HomeActivity;
 import com.team.a404.a404team.R;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class lobby extends AppCompatActivity {
 
     protected Button v_btn_iniciarSesion,v_btn_registro,v_btn_iniciarSesionCorreo;
     private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthState;
     private SignInButton v_btn_iniciarSesionGoogle;
     private FrameLayout v_pantalla;
+    private static final int RC_SIGN_IN = 123;
+    private static final String TAG = "GoogleActivity";
+    GoogleSignInClient mGoogleSignInClient;
+    /*
 
+    // Choose authentication providers
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+            new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build());
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +56,14 @@ public class lobby extends AppCompatActivity {
         getSupportActionBar().hide();
         v_btn_iniciarSesion = (Button) findViewById(R.id.btn_iniciarSesion);
         v_btn_registro = (Button) findViewById(R.id.btn_registro);
+        v_btn_iniciarSesionGoogle = (SignInButton) findViewById(R.id.btn_iniciarPorGoogle);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         /*
         int color = Color.parseColor("#7C0032");
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -43,6 +79,14 @@ public class lobby extends AppCompatActivity {
             startActivity(intent1);
 
         }
+        firebaseAuthState = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null){
+                    startActivity(new Intent(lobby.this, HomeActivity.class));
+                }
+            }
+        };
 
         v_btn_iniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +98,7 @@ public class lobby extends AppCompatActivity {
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.parseColor("#25000000")));
 
                 dialog.setContentView(R.layout.login_dialog);
+
                 dialog.show();
                 dialog.setCanceledOnTouchOutside(true);
                 v_btn_iniciarSesionCorreo = (Button) dialog.findViewById(R.id.btn_iniciarPorCorreo);
@@ -78,6 +123,12 @@ public class lobby extends AppCompatActivity {
                         overridePendingTransition(R.anim.left_in, R.anim.zoom_back_out);
                     }
                 });
+                v_btn_iniciarSesionGoogle.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        signIn();
+                    }
+                });
             }
         });
         v_btn_registro.setOnClickListener(new View.OnClickListener() {
@@ -88,4 +139,62 @@ public class lobby extends AppCompatActivity {
             }
         });
     }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+        firebaseAuth.addAuthStateListener(firebaseAuthState);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+/*
+    public void onStart() {
+        super.onStart();
+
+        firebaseAuth.addAuthStateListener(firebaseAuthState);
+    }*/
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(lobby.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
 }
